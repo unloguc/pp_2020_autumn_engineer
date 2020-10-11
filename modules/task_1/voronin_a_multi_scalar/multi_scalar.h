134379910dcc503d40be49cbe838725c69b3a29f
VoroninAA <voralexan@gmail.com>
2020-10-11 19:26:46 -0400

    Воронин Алексей. Задача 1. Скалярное произведение векторов.

---
+++ modules/task_1/voronin_a_multi_scalar/multi_scalar.h
@@ -0,0 +1,13 @@
+// Copyright 2020 Voronin Aleksey
+#ifndef MODULES_TASK_1_VORONIN_A_MULTI_SCALAR_MULTI_SCALAR_H_
+#define MODULES_TASK_1_VORONIN_A_MULTI_SCALAR_MULTI_SCALAR_H_
+
+#include <vector>
+#include <string>
+
+std::vector<int> getRandomVector(int  size);
+int getParallelOperations(std::vector<int> global_vec, std::vector<int> second_global_vec,
+                          int count_size_vector);
+int getSequentialOperations(std::vector<int> vec, std::vector<int> second_vec);
+
+#endif  // MODULES_TASK_1_VORONIN_A_MULTI_SCALAR_MULTI_SCALAR_H_
