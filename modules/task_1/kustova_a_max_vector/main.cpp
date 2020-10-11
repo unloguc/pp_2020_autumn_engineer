4c0a95537d672b8b4a9576987bf7c60eb70fbe22
KustovaAI <kustova_nastenka@list.ru>
2020-10-11 17:35:11 -0400

    fnfn

--- modules/task_1/kustova_a_max_vector/main.cpp
+++ modules/task_1/kustova_a_max_vector/main.cpp
@@ -93,6 +93,24 @@ int main(int argc, char** argv) {
 #include <gtest/gtest.h>
 #include <vector>
 #include "./max_vector.h"
+TEST(Parallel_Operations_MPI, Test_can_count_Max) {
+    std::cout << 1;
+    int rank;
+    std::cout << 2;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::cout << 3;
+    std::vector<int> global_vec;
+    std::cout << 4;
+    const int count_size_vector = 100;
+    std::cout << 5;
+    if (rank == 0) {
+    std::cout << 6;
+        global_vec = getRandomVector(count_size_vector);
+    std::cout << 7;
+    }
+    std::cout << 8;
+    ASSERT_NO_THROW(getParallelOperations(global_vec, count_size_vector, "max"));
+}
 
 TEST(Parallel_Operations_MPI, Test_Max) {
     int rank;
@@ -112,6 +130,32 @@ TEST(Parallel_Operations_MPI, Test_Max) {
         ASSERT_EQ(reference_max, global_max);
     }
 }
+
+
+TEST(Parallel_Operations_MPI, Test_can_find_local_max) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec = {3, 4, 9, -6};
+    if (rank == 0) {
+        int reference_max = getSequentialOperations(global_vec, "max");
+        ASSERT_EQ(9, reference_max);
+    }
+}
+
+
+TEST(Parallel_Operations_MPI, Test_can_gen_random_vector) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec1;
+    std::vector<int> global_vec2;
+    const int count_size_vector = 100;
+
+    if (rank == 0) {
+        ASSERT_NO_THROW(getRandomVector(count_size_vector));
+    }
+}
+
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
