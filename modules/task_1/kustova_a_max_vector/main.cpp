859d5d5160ac73a2dd6755b3058ccde9843115ae
KustovaAI <kustova_nastenka@list.ru>
2020-10-11 16:13:22 -0400

    hkhjhj

--- modules/task_1/kustova_a_max_vector/main.cpp
+++ modules/task_1/kustova_a_max_vector/main.cpp
@@ -94,42 +94,6 @@ int main(int argc, char** argv) {
 #include <vector>
 #include "./max_vector.h"
 
-TEST(Parallel_Operations_MPI, Test_Sum) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> global_vec;
-    const int count_size_vector = 100;
-
-    if (rank == 0) {
-        global_vec = getRandomVector(count_size_vector);
-    }
-
-    int global_sum = getParallelOperations(global_vec, count_size_vector, "+");
-
-    if (rank == 0) {
-        int reference_sum = getSequentialOperations(global_vec, "+");
-        ASSERT_EQ(reference_sum, global_sum);
-    }
-}
-
-TEST(Parallel_Operations_MPI, Test_Diff) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> global_vec;
-    const int count_size_vector = 100;
-
-    if (rank == 0) {
-        global_vec = getRandomVector(count_size_vector);
-    }
-
-    int global_diff = getParallelOperations(global_vec, count_size_vector, "-");
-
-    if (rank == 0) {
-        int reference_diff = getSequentialOperations(global_vec, "-");
-        ASSERT_EQ(reference_diff, global_diff);
-    }
-}
-
 TEST(Parallel_Operations_MPI, Test_Max) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
--- modules/task_1/kustova_a_max_vector/max_vector.cpp
+++ modules/task_1/kustova_a_max_vector/max_vector.cpp
@@ -79,19 +79,9 @@ std::vector<int> getRandomVector(int sz) {
 int getSequentialOperations(std::vector<int> vec, std::string ops) {
     const int  sz = vec.size();
     int reduction_elem = 0;
-    if (ops == "+") {
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem += vec[i];
-        }
-    } else if (ops == "-") {
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem -= vec[i];
-        }
-    } else if (ops == "max") {
-        reduction_elem = vec[0];
-        for (int  i = 1; i < sz; i++) {
-            reduction_elem = std::max(reduction_elem, vec[i]);
-        }
+    reduction_elem = vec[0];
+    for (int  i = 1; i < sz; i++) {
+        reduction_elem = std::max(reduction_elem, vec[i]);
     }
     return reduction_elem;
 }
@@ -122,8 +112,7 @@ int getParallelOperations(std::vector<int> global_vec,
     int global_sum = 0;
     int local_sum = getSequentialOperations(local_vec, ops);
     MPI_Op op_code;
-    if (ops == "+" || ops == "-") { op_code = MPI_SUM; }
-    if (ops == "max") { op_code = MPI_MAX; }
+    op_code = MPI_MAX;
     MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, op_code, 0, MPI_COMM_WORLD);
     return global_sum;
 }
