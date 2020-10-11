2e3819c808f98e956651dfe8c8345c9794efc231
KustovaAI <kustova_nastenka@list.ru>
2020-10-11 15:51:49 -0400

    old

--- modules/task_1/kustova_a_max_vector/main.cpp
+++ modules/task_1/kustova_a_max_vector/main.cpp
@@ -1,12 +1,12 @@
 // Copyright 2020 Kustova Anastasiya
-#include <gtest-mpi-listener.hpp>
+/*#include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <vector>
 #include <iostream>
 #include "./max_vector.h"
 
 
-/*TEST(Parallel_Operations_MPI, Test_can_count_Max) {
+TEST(Parallel_Operations_MPI, Test_can_count_Max) {
     std::cout << 1;
     int rank;
     std::cout << 2;
@@ -23,7 +23,7 @@
     }
     std::cout << 8;
     ASSERT_NO_THROW(getParallelOperations(global_vec, count_size_vector, "max"));
-}*/
+}
 
 TEST(Parallel_Operations_MPI, Test_Max) {
     int rank;
@@ -43,7 +43,7 @@ TEST(Parallel_Operations_MPI, Test_Max) {
         ASSERT_EQ(reference_max, global_max);
     }
 }
-/*
+
 TEST(Parallel_Operations_MPI, Test_can_find_local_max) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -72,8 +72,82 @@ TEST(Parallel_Operations_MPI, Test_throw_empty_vector) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     std::vector<int> global_vec;
     ASSERT_ANY_THROW(getParallelOperations(global_vec, 0, "max"));
-}*/
+}
 
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}*/
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./max_vector.h"
+
+TEST(Parallel_Operations_MPI, Test_Sum) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 100;
+
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+
+    int global_sum = getParallelOperations(global_vec, count_size_vector, "+");
+
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec, "+");
+        ASSERT_EQ(reference_sum, global_sum);
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_Diff) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 100;
+
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+
+    int global_diff = getParallelOperations(global_vec, count_size_vector, "-");
+
+    if (rank == 0) {
+        int reference_diff = getSequentialOperations(global_vec, "-");
+        ASSERT_EQ(reference_diff, global_diff);
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_Max) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 100;
+
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+
+    int global_max;
+    global_max = getParallelOperations(global_vec, count_size_vector, "max");
+
+    if (rank == 0) {
+        int reference_max = getSequentialOperations(global_vec, "max");
+        ASSERT_EQ(reference_max, global_max);
+    }
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
