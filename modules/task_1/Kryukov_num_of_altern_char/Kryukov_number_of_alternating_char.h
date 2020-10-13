11956217a589111c926d9bc956684720e7e64962
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-10-13 21:26:50 +0300

    WIthout test

--- modules/task_1/Kryukov_num_of_altern_char/Kryukov_number_of_alternating_char.h
+++ modules/task_1/Kryukov_num_of_altern_char/Kryukov_number_of_alternating_char.h
@@ -1,1 +1,11 @@
-#pragma once
+// Copyright 2020 Kryukov Sergey
+#ifndef MODULES_TASK_1_KRYUKOV_NUM_OF_ALTERN_CHAR_KRYUKOV_NUMBER_OF_ALTRNATING_CHAR_H_
+#define MODULES_TASK_1_KRYUKOV_NUM_OF_ALTERN_CHAR_KRYUKOV_NUMBER_OF_ALTRNATING_CHAR_H_
+
+#include <vector>
+
+std::vector<int> getRandomVector(int size);
+int getNUMalternCHAR(std::vector<int> vec);
+int getParallelNUMalternCHAR(std::vector<int> general_vec, int size_vector);
+
+#endif  // MODULES_TASK_1_KRYUKOV_NUM_OF_ALTERN_CHAR_KRYUKOV_NUMBER_OF_ALTRNATING_CHAR_H__NUMBER_OF_ALTRNATING_CHAR_H_
--- modules/task_1/Kryukov_num_of_altern_char/main.cpp
+++ modules/task_1/Kryukov_num_of_altern_char/main.cpp
@@ -1,9 +1,23 @@
-﻿
+﻿// Copyright 2020 Kryukov Sergey
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./Kryukov_number_of_alternating_char.h"
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	MPI_Init(&argc, &argv);
+
+	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+	::testing::TestEventListeners& listeners =
+		::testing::UnitTest::GetInstance()->listeners();
+
+	listeners.Release(listeners.default_result_printer());
+	listeners.Release(listeners.default_xml_generator());
+
+	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+	return RUN_ALL_TESTS();
 }
-
