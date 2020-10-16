db9f18a178ffc32f619d82e8c84f134af4c2c03d
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-10-16 12:18:55 +0300

    correct for gcc build 1

--- modules/task_1/Kryukov_num_of_altern_char/Kryukov_number_of_alternating_char.cpp
+++ modules/task_1/Kryukov_num_of_altern_char/Kryukov_number_of_alternating_char.cpp
@@ -16,45 +16,44 @@ std::vector<int> getRandomVector(int size) {
 }
 
 int getNUMalternCHAR(std::vector<int> vec) {
-	const int size = vec.size();
-	int number = 0;
-	for (int i = 0; i < size - 1; i++) {
-		if (vec[i] < 0 && vec[i + 1] >= 0)
-			number += 1;
-		else if (vec[i] >= 0 && vec[i + 1] < 0)
-			number += 1;
-		else
-			continue;
-	}
-	return number;
+    const int size = vec.size();
+    int number = 0;
+    for (int i = 0; i < size - 1; i++) {
+    	if (vec[i] < 0 && vec[i + 1] >= 0)
+    		number += 1;
+    	else if (vec[i] >= 0 && vec[i + 1] < 0)
+    		number += 1;
+    	else
+    		continue;
+    }
+    return number;
 }
 
 int getParallelNUMalternCHAR(std::vector<int> general_vec, int size_vector) {
-	int size, rank;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	const int delta = size_vector / size;
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const int delta = size_vector / size;
 
-	if (rank == 0) {
-		for (int process = 0; process < size-1; process++)
-			MPI_Send(&general_vec[0] + process * delta, delta,
-				MPI_INT, process, 0, MPI_COMM_WORLD);
-	}
+    if (rank == 0) {
+    	for (int process = 0; process < size-1; process++)
+    		MPI_Send(&general_vec[0] + process * delta, delta,
+    			MPI_INT, process, 0, MPI_COMM_WORLD);
+    }
 
-	std::vector<int> local_vec(delta);
-	if (rank == 0) {
-		local_vec = std::vector<int>(general_vec.begin(),
-			general_vec.begin() + delta);
-	}
-	else {
-		MPI_Status status;
+    std::vector<int> local_vec(delta);
+    if (rank == 0) {
+    	local_vec = std::vector<int>(general_vec.begin(),
+    		general_vec.begin() + delta);
+    } else {
+        MPI_Status status;
 		MPI_Recv(&local_vec[0], delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	}
+    }
 
-	int final_altern = 0;
-	int local_altern = getNUMalternCHAR(local_vec);
-	MPI_Op op_code;
-	op_code = MPI_SUM;
-	MPI_Reduce(&local_altern, &final_altern, 1, MPI_INT, op_code, 0, MPI_COMM_WORLD);
-	return final_altern;
+    int final_altern = 0;
+    int local_altern = getNUMalternCHAR(local_vec);
+    MPI_Op op_code;
+    op_code = MPI_SUM;
+    MPI_Reduce(&local_altern, &final_altern, 1, MPI_INT, op_code, 0, MPI_COMM_WORLD);
+    return final_altern;
 }
