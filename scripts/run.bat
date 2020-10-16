97142a83df6c7892aa9f64a61a361604d7a91d26
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-10-16 21:21:20 +0300

    trying to fix build

--- scripts/run.bat
+++ scripts/run.bat
@@ -38,12 +38,3 @@ for /r "." %%a in (build\bin\*_mpi.exe) do (
 )
 
 
-for /r "." %%a in (build\bin\*_Kryukov_number_of_alternating_char.exe) do (
-    echo -------------------------------------
-    echo %%~na
-    echo -------------------------------------
-    for /l %%x in (1, 1, 1) do (
-        "C:\Users\HP-PC\Desktop\лаба 1 mpi\pp_2020_autumn_engineer\modules\task_1\Kryukov_num_of_altern_char\Kryukov_number_of_alternating_char\DebugKryukov_number_of_alternating_char.exe" -np 4 %%~fa || exit /b 1
-    )
-)
-
