5b0d1f4a3411046f0ffcf93dc2a573ee61abe870
Denis <alehin.de@gmail.com>
2020-10-17 16:07:39 +0300

    Fix

--- modules/task_1/alekhin_d_integration_trapezoidal_method/main.cpp
+++ modules/task_1/alekhin_d_integration_trapezoidal_method/main.cpp
@@ -47,14 +47,6 @@ TEST(Integration_Test, Integration_Return_Right_Value_3) {
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   MPI_Init(&argc, &argv);
-  
-  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-  ::testing::TestEventListeners& listeners =
-    ::testing::UnitTest::GetInstance()->listeners();
 
-  listeners.Release(listeners.default_result_printer());
-  listeners.Release(listeners.default_xml_generator());
-
-  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
   return RUN_ALL_TESTS();
 }
