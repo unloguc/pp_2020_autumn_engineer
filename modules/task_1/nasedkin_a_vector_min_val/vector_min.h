2e01bf991d68dbec99c516884151782ff99093f9
Nikita Nasedkin <n.nasedkin@gmail.com>
2020-10-19 17:18:37 +0300

    Finished

--- modules/task_1/nasedkin_a_vector_min_val/vector_min.h
+++ modules/task_1/nasedkin_a_vector_min_val/vector_min.h
@@ -7,6 +7,6 @@
 
 std::vector<int> rndVector(int vector_size);
 int ParallelMinimum(std::vector<int> global_vec, int vector_size);
-int SequentialMininum(std::vector<int> vec);
+int SequentialMininum(std::vector<int> vect);
 
 #endif  // MODULES_TASK_1_NASEDKIN_A_VECTOR_MIN_VAL_VECTOR_MIN_H_
