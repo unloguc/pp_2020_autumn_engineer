94683bfc060f150a9800d123176a661e753b36fd
OganyanRV <47751491+OganyanRV@users.noreply.github.com>
2020-10-20 16:30:16 +0300

    final commit

--- modules/task_1/oganyan_r_letters_amount/letters_amount.h
+++ modules/task_1/oganyan_r_letters_amount/letters_amount.h
@@ -1,15 +1,13 @@
-//
-// Created by OgRob on 19.10.2020.
-//
+// Copyright 2020 Oganyan Robert
 
-#ifndef PP_2020_AUTUMN_ENGINEER_MODULES_TASK_1_OGANYAN_R_LETTERS_AMOUNT_LETTERS_AMOUNT_H_
-#define PP_2020_AUTUMN_ENGINEER_MODULES_TASK_1_OGANYAN_R_LETTERS_AMOUNT_LETTERS_AMOUNT_H_
+#ifndef MODULES_TASK_1_OGANYAN_R_LETTERS_AMOUNT_LETTERS_AMOUNT_H_
+#define MODULES_TASK_1_OGANYAN_R_LETTERS_AMOUNT_LETTERS_AMOUNT_H_
 
 #include <string>
 
-std::string getRandomString(int size);
-int getSequentialOperations(std::string str);
-int getParallelOperations(std::string global_str, int size_str);
+std::string getRandomString(const int& size);
+int getSequentialOperations(const std::string& str);
+int getParallelOperations(const std::string& global_str, const int& size_str);
 
 
-#endif //PP_2020_AUTUMN_ENGINEER_MODULES_TASK_1_OGANYAN_R_LETTERS_AMOUNT_LETTERS_AMOUNT_H_
+#endif  //  MODULES_TASK_1_OGANYAN_R_LETTERS_AMOUNT_LETTERS_AMOUNT_H_
--- modules/task_1/oganyan_r_letters_amount/main.cpp
+++ modules/task_1/oganyan_r_letters_amount/main.cpp
@@ -1,26 +1,24 @@
-//
-// Created by OgRob on 19.10.2020.
-//
+// Copyright 2020 Oganyan Robert
 
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <string>
-#include "./letters_amount.h"
+#include "../../../modules/task_1/oganyan_r_letters_amount/letters_amount.h"
 
-void CustomTest(const int test_size) {
+void CustomTest(const int& test_size) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   std::string global_str;
-  if (rank==0) {
+  if (rank == 0) {
     global_str = getRandomString(test_size);
   }
   auto paral_ans = getParallelOperations(global_str, test_size);
-  if (rank==0) {
+  if (rank == 0) {
     auto seq_ans = getSequentialOperations(global_str);
     ASSERT_EQ(seq_ans, paral_ans);
   }
-
 }
+
 TEST(Parallel_Operations_MPI, Test_tiny) {
   CustomTest(1);
 }
