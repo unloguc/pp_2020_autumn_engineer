0624ffd373057fd6e684ca290c67357509530194
Anisimova Polina <polinaanis05@mail.ru>
2020-10-21 07:33:53 -0400

    Anisimova Polina. Task 1. Version 4

--- modules/task_1/anisimova_p_wrong_vector/wrong_vector.cpp
+++ modules/task_1/anisimova_p_wrong_vector/wrong_vector.cpp
@@ -22,7 +22,7 @@ std::vector<int> getRandomVector(int sz) {
   if (sz == 0) return 0;
 
   int sum = 0;
-  for (int i = 0; i < sz - 1; i++) {
+  for (int i = 0; i < sz ; i++) {
      if (vec[i] > vec[i + 1])
              sum++;
   }
