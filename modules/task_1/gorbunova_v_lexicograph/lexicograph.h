370fef180d12197904f5c76f0a3c096fec9a4b7d
unknown <gorbunova-valeriya@mail.ru>
2020-10-22 19:09:53 +0300

    gorbunova_v_lexicograph_first_commit

---
+++ modules/task_1/gorbunova_v_lexicograph/lexicograph.h
@@ -0,0 +1,5 @@
+#include <string>
+
+std::string generateString(const int length);
+bool getSecuentialOperation(const std::string part);
+bool getParallelOperation(const std::string s1, const std::string s2);
