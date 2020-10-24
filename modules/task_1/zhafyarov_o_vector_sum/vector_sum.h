afdfbb6166df543ad868f740bbc2735e899ec2f3
Oleg <olejka647@gmail.com>
2020-10-24 17:54:18 +0300

    Release 4.0

--- modules/task_1/zhafyarov_o_vector_sum/vector_sum.h
+++ modules/task_1/zhafyarov_o_vector_sum/vector_sum.h
@@ -5,10 +5,10 @@
 #include <vector>
 
 
-std::vector <int> getRandomVector(int Size);
+std::vector <int> GetRandomVector(int size);
 
-int getParallelOperations(std::vector <int> vec, int Size);
+int GetParallelSum(std::vector <int> vec, int size);
 
-int getSequentialOperations(std::vector<int> vec);
+int GetSequentialSum(std::vector<int> vec);
 
 #endif  // MODULES_TASK_1_ZHAFYAROV_O_VECTOR_SUM_VECTOR_SUM_H_
