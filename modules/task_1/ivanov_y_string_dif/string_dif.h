ab129217da6091c434ac6d6f6aee8a2314193456
IvanovYaAl <47712929+IvanovYaAl@users.noreply.github.com>
2020-10-28 16:41:37 +0300

    Ver 1.0 fix tabs 1

--- modules/task_1/ivanov_y_string_dif/string_dif.h
+++ modules/task_1/ivanov_y_string_dif/string_dif.h
@@ -8,4 +8,4 @@ std::string generateString(int n);
 int getParallelDif(const std::string str1, const std::string str2, const int size);
 int getLocalDif(const std::string str1, const std::string str2, const int size);
 
-#endif //MODULES_TASK_1_IVANOV_Y_STRING_DIF_STRING_DIF_H_
+#endif  // MODULES_TASK_1_IVANOV_Y_STRING_DIF_STRING_DIF_H_
