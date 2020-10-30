118ac5b4fd68fbd0a8b5cb8b8888618af9c472ea
kartash111 <ilyakochankov@MBP-Ilya.Dlink>
2020-10-30 13:16:00 +0300

    fixed bool

--- modules/task_1/kartashev_v_lexical_order/lexical_order.cpp
+++ modules/task_1/kartashev_v_lexical_order/lexical_order.cpp
@@ -39,7 +39,7 @@ int get_parallel_operations(const char* str_1, const char* str_2, int length_1, 
         if (rank == 0) {
             return get_sequential_operations(str_1, str_2, length_1, length_2);
         } else {
-            return false;
+            return 0;
         }
     }
 
