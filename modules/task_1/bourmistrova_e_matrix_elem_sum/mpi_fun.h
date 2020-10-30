3009305de82670a72a1263d18ea3af5198537c76
BurmistrovaEO <tigrettokat@mail.ru>
2020-10-30 21:16:03 +0300

    fixed code style

--- modules/task_1/bourmistrova_e_matrix_elem_sum/mpi_fun.h
+++ modules/task_1/bourmistrova_e_matrix_elem_sum/mpi_fun.h
@@ -1,12 +1,13 @@
-#ifndef MODULES_TASK_1_BOURMISTROVA_E_MATRIX_ELEM_SUM_MPI_FUN_H
-#define MODULES_TASK_1_BOURMISTROVA_E_MATRIX_ELEM_SUM_MPI_FUN_H
+// Copyright 2020 Ekaterina Burmistrova
+#ifndef MODULES_TASK_1_BOURMISTROVA_E_MATRIX_ELEM_SUM_MPI_FUN_H_
+#define MODULES_TASK_1_BOURMISTROVA_E_MATRIX_ELEM_SUM_MPI_FUN_H_
 
 #include <vector>
 
 std::vector<int> getRandomMatrix(int  sz1, int sz2);
 int Sequential_method(std::vector<int> vect);
 int Parallel_method(std::vector<int> matr,
-	int n_lin, int n_col);
+        int n_lin, int n_col);
 
-#endif  // MODULES_TASK_1_BOURMISTROVA_E_MATRIX_ELEM_SUM_MPI_FUN_H
+#endif  // MODULES_TASK_1_BOURMISTROVA_E_MATRIX_ELEM_SUM_MPI_FUN_H_
 
