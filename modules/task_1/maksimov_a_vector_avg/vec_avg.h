a474856a22335732d4017f782450ceb2d7d72159
MaksimovAndrey <andreymak123@mail.ru>
2020-10-30 16:50:23 +0300

    Task 1

---
+++ modules/task_1/maksimov_a_vector_avg/vec_avg.h
@@ -0,0 +1,10 @@
+// Copyright 2020 Maksimov Andrey
+#ifndef MODULES_TASK_1_MAKSIMOV_A_VECTOR_AVG_H_
+#define MODULES_TASK_1_MAKSIMOV_A_VECTOR_AVG_H_
+
+#include <vector>
+
+double getVectorAvgNotParall(std::vector<int> vec, int vecSize);
+double getVectorAvg(std::vector<int> vec, int vecSize);
+
+#endif // MODULES_TASK_1_MAKSIMOV_A_VECTOR_AVG_H_
