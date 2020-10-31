ea2fa3395df35eb218a4e770a0e1af4c4d33848d
MaksimovAndrey <andreymak123@mail.ru>
2020-10-31 10:26:48 +0300

    idk what to do

--- modules/task_1/maksimov_a_vector_avg/main.cpp
+++ modules/task_1/maksimov_a_vector_avg/main.cpp
@@ -15,12 +15,8 @@ TEST(Parallel_Operations_MPI, Can_Generate_Vector) {
 
 TEST(Parallel_Operations_MPI, Test_Negative_VecSize) {
     const int vecSize = -10;
-
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank == 0) {
-        ASSERT_ANY_THROW(getRandomVector(vecSize));
-    }
+    
+    ASSERT_ANY_THROW(getRandomVector(vecSize));
 }
 
 TEST(Parallel_Operations_MPI, Test_Size_1) {
