b0b78d10782fcd45600d71b93516c636ac675c61
MerywmElf <meryem.elfahli@gmail.com>
2020-10-31 12:31:09 +0300

    first commit

--- modules/task_1/elfahli_sents_in_line/sents_mpi.h
+++ modules/task_1/elfahli_sents_in_line/sents_mpi.h
@@ -1,6 +1,6 @@
 // Copyright 2020 el fahli meryem
-#ifndef MODULES_TASK_1_ELFAHLI_A_SENTS_IN_LINE_SENTS_MPI_H_
-#define MODULES_TASK_1_ELFAHLI_A_SENTS_IN_LINE_SENTS_MPI_H_
+#ifndef MODULES_TASK_1_ELFAHLI_SENTS_IN_LINE_SENTS_MPI_H_
+#define MODULES_TASK_1_ELFAHLI_SENTS_IN_LINE_SENTS_MPI_H_
 
 #include <vector>
 
@@ -9,4 +9,4 @@ int getParallelSumSents(std::vector<char> g_line, int s_line);
 int getSeqSumSents(std::vector<char> l);
 
 
-#endif  // MODULES_TASK_1_ELFAHLI_A_SENTS_IN_LINE_SENTS_MPI_H_
+#endif  // MODULES_TASK_1_ELFAHLI_SENTS_IN_LINE_SENTS_MPI_H_
