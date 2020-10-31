488c6a614d8745e87f8fc6233819a308fccb90ca
andrey-avdeev-gh <56041237+andrey-avdeev-gh@users.noreply.github.com>
2020-10-31 18:28:22 +0300

    Update rectangle_method.h

--- modules/task_1/avdeev_a_rectangle_method/rectangle_method.h
+++ modules/task_1/avdeev_a_rectangle_method/rectangle_method.h
@@ -1,10 +1,10 @@
 // Copyright 2020 Avdeev Andrey
-#ifndef MODULES_TASK_1_AVDEEV_A_RECTANGLES_METHOD_H_
-#define MODULES_TASK_1_AVDEEV_A_RECTANGLES_METHOD_H_
+#ifndef MODULES_TASK_1_AVDEEV_A_RECTANGLE_METHOD_RECTANGLE_METHOD_H_
+#define MODULES_TASK_1_AVDEEV_A_RECTANGLE_METHOD_RECTANGLE_METHOD_H_
 
 double getSequentialIntegration(double(*func)(double), const double& a, const double& b,
     const int& n);
 double getParallelIntegration(double(*func)(double), const double& a, const double& b,
     const int& n);
 
-#endif MODULES_TASK_1_AVDEEV_A_RECTANGLES_METHOD_H_
+#endif MODULES_TASK_1_AVDEEV_A_RECTANGLE_METHOD_RECTANGLE_METHOD_H_
