ddf695806013b67fa7bd36a941ea8b867207f84a
KustovaAI <kustova_nastenka@list.ru>
2020-11-03 17:40:31 -0500

    9 version

--- modules/task_2/kustova_a_jacoby/Jacoby.cpp
+++ modules/task_2/kustova_a_jacoby/Jacoby.cpp
@@ -52,104 +52,102 @@ double *Iteration_for_0_rank(int n, double *X_Old, double *Input_B, double *Bloc
     return Bloc_XX;
 }
 
-/*
 
-double* Parallel_Jacoby(int n, double eps)
-{
-    int size,rank,amountRowBloc, GlobalRowNo, index;
-//    clock_t begin, finish;
+double* Parallel_Jacoby(double Input_A[], double Input_B[], int n) {
+    int size, rank, amountRowBloc, GlobalRowNo, index;
+    double *ARecv, *BRecv, *Bloc_XX, *X_New, *X_Old, *Bloc_X;
 
-n = 2;
-double *Input_A, *Input_B, *ARecv, *BRecv, *Bloc_XX, *X_New, *X_Old, *Bloc_X;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    X_New = new double[n];
 
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    amountRowBloc = n / size;
+    ARecv = new double[amountRowBloc * n];
+    BRecv = new double[amountRowBloc];
 
-X_New = new double[n];
+    MPI_Scatter(Input_A, amountRowBloc * n, MPI_DOUBLE, ARecv, amountRowBloc * n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(Input_B, amountRowBloc, MPI_DOUBLE, BRecv, amountRowBloc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-if(rank==0)
-{
+    Bloc_X = new double[n];
+    X_New = new double[n];
+    X_Old = new double[n];
 
-Input_A = new double[n * n];
-Input_B = new double[n];
-//test1
-//Input_A[0] = 4; Input_A[1] = 2; Input_A[2] = 1; Input_A[3] = 3;
-//Input_B[0] = 1; Input_B[1] = -1;
-//test2
-Input_A[0] = 10; Input_A[1] = 1; Input_A[2] = -1; Input_A[3] = 1;
-Input_A[4] = 10; Input_A[5] = -1; Input_A[6] = -1; Input_A[7] = 1; Input_A[8] = 10;
-Input_B[0] = 11; Input_B[1] = 10; Input_B[2] = 10;
-//test3
-//Input_A[0] = 115; Input_A[1] = -20; Input_A[2] = -75; Input_A[3] = 15;
-//Input_A[4] = -50; Input_A[5] = -5; Input_A[6] = 6; Input_A[7] = 2; Input_A[8] = 20;
-//Input_B[0] = 20; Input_B[1] = -40; Input_B[2] = 28;
-
-}
-//begin = clock();
-MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-//MPI_Barrier(MPI_COMM_WORLD);
-    amountRowBloc = n / size;	//how much to each proc
-ARecv = new double[amountRowBloc * n];
-BRecv = new double[amountRowBloc];
-
-MPI_Scatter (Input_A, amountRowBloc * n, MPI_DOUBLE, ARecv, amountRowBloc * n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-MPI_Scatter (Input_B, amountRowBloc, MPI_DOUBLE, BRecv, amountRowBloc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-Bloc_X = new double[n];
-X_New = new double[n];
-X_Old = new double[n];
-
-for (int irow=0; irow < amountRowBloc; irow ++){
-    Bloc_X[irow] = BRecv[irow];
-}
-
-MPI_Allgather(Bloc_X, amountRowBloc, MPI_DOUBLE, X_New, amountRowBloc, MPI_DOUBLE, MPI_COMM_WORLD);
-int Iteration = 0;
-
-for(int irow=amountRowBloc * size; irow<n; irow++){
-	MPI_Allgather(&Input_B[irow], 1, MPI_DOUBLE, &X_New[irow], 1, MPI_DOUBLE, MPI_COMM_WORLD);
-}
-
-Bloc_XX = new double[n];
-do{
-    for(int irow = 0; irow < n;irow++){
-	X_Old[irow] = X_New[irow];
+    for (int irow = 0; irow < amountRowBloc; irow ++) {
+        Bloc_X[irow] = BRecv[irow];
     }
 
-    for(int irow = 0; irow < amountRowBloc; irow++){
-	GlobalRowNo = (rank * amountRowBloc) + irow;
-	Bloc_X[irow] = BRecv[irow];
-	index = irow * n;
-
-	for(int icol = 0; icol < n; icol++){
-	    if (icol != GlobalRowNo){
-		Bloc_X[irow] -= X_Old[icol] * ARecv[index + icol];
-	    }
-	}
-	Bloc_X[irow] = Bloc_X[irow] / ARecv[index + GlobalRowNo];
-    }
     MPI_Allgather(Bloc_X, amountRowBloc, MPI_DOUBLE, X_New, amountRowBloc, MPI_DOUBLE, MPI_COMM_WORLD);
 
-    if (rank == 0){
-	for(int irow = amountRowBloc * size; irow < n; irow ++){
-	    GlobalRowNo = irow;
-	    Bloc_XX[irow] = Input_B[irow];
-	    index = irow * n;
-	    for(int icol = 0; icol < n; icol++){
-		if (icol != GlobalRowNo){
-		    Bloc_XX[irow] -= X_Old[icol] * Input_A[index + icol];
-		}
-	    }
-	    Bloc_XX[irow] = Bloc_XX[irow] / Input_A[index + GlobalRowNo];
-	}
+    int Iteration = 0;
+    for (int irow = amountRowBloc * size; irow < n; irow++) {
+        MPI_Allgather(&Input_B[irow], 1, MPI_DOUBLE, &X_New[irow], 1, MPI_DOUBLE, MPI_COMM_WORLD);
     }
 
-    for(int irow = amountRowBloc * size; irow < n; irow ++){
-	MPI_Allgather(&Bloc_XX[irow], 1, MPI_DOUBLE, &X_New[irow], 1, MPI_DOUBLE, MPI_COMM_WORLD);
-    }
-    Iteration++;
-}while((Iteration < MAX_ITERATIONS) && (Distance(X_Old, X_New, n) >= eps));
-//finish = clock();
+    Bloc_XX = new double[n];
+    do {
+        for (int irow = 0; irow < n; irow++) {
+            X_Old[irow] = X_New[irow];
+        }
+        for (int irow = 0; irow < amountRowBloc; irow++) {
+            GlobalRowNo = (rank * amountRowBloc) + irow;
+            Bloc_X[irow] = BRecv[irow];
+            index = irow * n;
 
-return X_New;
-}*/
+            for (int icol = 0; icol < n; icol++) {
+                if (icol != GlobalRowNo) {
+                    Bloc_X[irow] -= X_Old[icol] * ARecv[index + icol];
+                }
+            }
+            Bloc_X[irow] = Bloc_X[irow] / ARecv[index + GlobalRowNo];
+        }
+        MPI_Allgather(Bloc_X, amountRowBloc, MPI_DOUBLE, X_New, amountRowBloc, MPI_DOUBLE, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            for (int irow = amountRowBloc * size; irow < n; irow ++) {
+                GlobalRowNo = irow;
+                Bloc_XX[irow] = Input_B[irow];
+                index = irow * n;
+                for (int icol = 0; icol < n; icol++) {
+                    if (icol != GlobalRowNo) {
+                        Bloc_XX[irow] -= X_Old[icol] * Input_A[index + icol];
+                    }
+                }
+                Bloc_XX[irow] = Bloc_XX[irow] / Input_A[index + GlobalRowNo];
+            }
+        }
+        for (int irow = amountRowBloc * size; irow < n; irow ++) {
+            MPI_Allgather(&Bloc_XX[irow], 1, MPI_DOUBLE, &X_New[irow], 1, MPI_DOUBLE, MPI_COMM_WORLD);
+        }
+        Iteration++;
+    } while ((Iteration < MAX_ITERATIONS) && (Distance(X_Old, X_New, n) >= 0.0001));
+// finish = clock();
+
+    return X_New;
+}
+
+
+double* Sequential_Jacoby(double Input_A[], double Input_B[], int n) {
+    double *X_New, *X_Old;
+    int index = 0, Iteration = 0, GlobalRowNo;
+    X_New = new double[n];
+    X_Old = new double[n];
+
+    do {
+        for (int irow = 0; irow < n; irow++) {
+            X_Old[irow] = X_New[irow];
+        }
+        for (int irow = 0; irow < n; irow ++) {
+            GlobalRowNo = irow;
+            X_New[irow] = Input_B[irow];
+            index = irow * n;
+            for (int icol = 0; icol < n; icol++) {
+                if (icol != GlobalRowNo) {
+                   X_New[irow] -= X_Old[icol] * Input_A[index + icol];
+                }
+            }
+        X_New[irow] = X_New[irow] / Input_A[index + GlobalRowNo];
+        }
+        Iteration++;
+    }while((Iteration < MAX_ITERATIONS) && (Distance(X_Old, X_New, n) >= 0.0001));
+    return X_New;
+}
