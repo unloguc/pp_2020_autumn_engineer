1987d55d81bb4b496946ae0eb4ad5d0e6c50bd66
Igor <kdikasz@mail.ru>
2020-11-03 05:09:33 +0300

    Version 7. Var 16

--- modules/task_1/rachin_i_max_val_mat_columns/main.cpp
+++ modules/task_1/rachin_i_max_val_mat_columns/main.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include "./max_of_clmns_mtrx.h"
+
 TEST(Parallel_Operations_MPI, Test_manual_matrix_seq_max) {
     std::vector<int> test{ 23, 100, 59, 14, 25, 100,
                           100, 29, 49, 100, 38, 15,
@@ -12,6 +13,19 @@ TEST(Parallel_Operations_MPI, Test_manual_matrix_seq_max) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
         ASSERT_EQ(getSequentialMaxOfClmns(test, 3, 6), result);
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_manual_matrix_parallel_max) {
+    std::vector<int> test{ 23, 100, 59, 14, 25, 100,
+                           100, 29, 49, 100, 38, 15,
+                           25, 31, 100, 29, 100, 28 };
+    std::vector<int> result{ 100, 100, 100, 100, 100, 100 };
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> resultParallel = getParallelMaxOfClmns(test, 3, 6);
+    if (rank == 0) {
+        ASSERT_EQ(resultParallel, result);
     }
 }
 
@@ -53,7 +67,6 @@ TEST(Parallel_Operations_MPI, Test_matrix_negative_size) {
         ASSERT_EQ(test, clear);
     }
 }
-
 
 TEST(Parallel_Operations_MPI, Test_matrix_1000x1000_size) {
     std::vector<int> test;
--- modules/task_1/rachin_i_max_val_mat_columns/max_of_clmns_mtrx.cpp
+++ modules/task_1/rachin_i_max_val_mat_columns/max_of_clmns_mtrx.cpp
@@ -7,7 +7,6 @@
 #include <algorithm>
 #include "../../modules/task_1/rachin_i_max_val_mat_columns/max_of_clmns_mtrx.h"
 
-
 std::vector<int> getRandomMatrix(int rows, int clmns) {
     if (rows <= 0 || clmns <= 0) {
         return std::vector<int>();
@@ -41,43 +40,49 @@ std::vector<int> getSequentialMaxOfClmns(std::vector<int> mx, int rows, int clmn
     return maxValues;
 }
 
-std::vector<int> getParallelMaxOfClmns(std::vector<int> mx, int rows, int clmns) {
+std::vector<int> getParallelMaxOfClmns(std::vector<int> localMtrx, int rows, int clmns) {
     if (rows <= 0 || clmns <= 0) {
         return std::vector<int>();
     }
     int size, rank;
+    std::vector<int> maxValues(clmns);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int delta = clmns / size;
-    int tail = clmns % size;
-    std::vector<int> localMtrx;
-    std::vector<int> localMain(delta * rows);
-    std::vector<int> localTail(tail * rows);
-    std::vector<int>  maxValues(clmns);  // result_vector
-    if (rank == 0) {
-        localMtrx = mx;
-        for (int proc = 1; proc < size; proc++) {
-            MPI_Send(&localMtrx[0] + proc * delta * rows, delta * rows, MPI_INT, proc, 0, MPI_COMM_WORLD);
-        }
-    }
-    if (rank == 0) {
-        localMain = std::vector<int>(localMtrx.begin(), localMtrx.begin() + delta*rows);
-        if (tail != 0) {
-            localTail = std::vector<int>(localMtrx.end() - tail * rows, localMtrx.end());
+    if (size > clmns) {
+        if (rank == 0) {
+            maxValues = getSequentialMaxOfClmns(localMtrx, rows, clmns);
         }
     } else {
-        MPI_Status status;
-        MPI_Recv(&localMain[0], delta*rows, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-    }
-    localMain = getSequentialMaxOfClmns(localMain, rows, delta);
-    if (rank == 0 && tail != 0) {
-        localTail = getSequentialMaxOfClmns(localTail, rows, tail);
-    }
-    MPI_Gather(&localMain[0], delta, MPI_INT, &maxValues[0], delta, MPI_INT, 0, MPI_COMM_WORLD);
-    if (rank == 0 && tail != 0) {
-        for (int i = 0; i < tail; i++) {
-            maxValues[clmns - tail + i] = localTail[i];
+        int delta = clmns / size;
+        int tail = clmns % size;
+        std::vector<int> localMain(delta * rows);
+        std::vector<int> localTail(tail * rows);
+        if (rank == 0) {
+            for (int proc = 1; proc < size; proc++) {
+                for (int i = 0; i < rows; i++) {
+                    MPI_Send(&localMtrx[proc * delta + i * clmns], delta, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                }
+            }
+            for (int i = 0; i < rows; i++) {
+                localMain.insert(localMain.begin() + i * delta, localMtrx.begin() + i * clmns, localMtrx.begin() + i * clmns + delta);
+            }
+            if (tail != 0) {
+                for (int i = 0; i < rows; i++) {
+                    localTail.insert(localTail.begin() + i * tail, localMtrx.begin() + delta * size + clmns * i, localMtrx.begin() + delta * size + clmns * i + tail);
+                }
+                localTail = getSequentialMaxOfClmns(localTail, rows, tail);
+                for (int i = 0; i < tail; i++) {
+                    maxValues[clmns - tail + i] = localTail[i];
+                }
+            }
+        } else {
+            MPI_Status status;
+            for (int i = 0; i < rows; i++) {
+                MPI_Recv(&localMain[0 + i * delta], delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            }
         }
+        localMain = getSequentialMaxOfClmns(localMain, rows, delta);
+        MPI_Gather(&localMain[0], delta, MPI_INT, &maxValues[0], delta, MPI_INT, 0, MPI_COMM_WORLD);
     }
     return maxValues;
 }
