8f442d236c9b5b851add8fffc4869bdba1e25242
KustovaAI <kustova_nastenka@list.ru>
2020-11-05 12:56:07 -0500

    17 version

--- modules/task_2/kustova_a_jacoby/start.sh
+++ modules/task_2/kustova_a_jacoby/start.sh
@@ -1,6 +1,7 @@
 NAME=$(pwd|tr / "\n"|tail |tail -1)_mpi
 cd ../../../build
 cmake -D USE_SEQ=ON -D USE_MPI=ON -D USE_OMP=ON -D USE_TBB=ON -D USE_STD=ON -D USE_LATEX=ON ..
-cmake —build . —config RELEASE
+cmake --build . --config RELEASE
 cd bin/
-./$NAME
+#./$NAME
+mpiexec -n 2 $NAME
