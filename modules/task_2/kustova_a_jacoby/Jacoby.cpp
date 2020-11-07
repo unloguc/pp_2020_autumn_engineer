12dd29e099c7b4e25ae58529b50b2881f82beddb
KustovaAI <kustova_nastenka@list.ru>
2020-11-07 03:06:36 -0500

    35 version

--- modules/task_2/kustova_a_jacoby/Jacoby.cpp
+++ modules/task_2/kustova_a_jacoby/Jacoby.cpp
@@ -59,20 +59,22 @@ double *Iteration_for_0_rank(int n, double *X_Old, double *Input_B, double *Bloc
 }
 
 std::vector<double> Parallel_Jacoby(std::vector<double> A, std::vector<double> B, int n, double eps) {
-    int size, rank, amountRowBloc, GlobalRowNo;
+    int size, rank, amountRowBloc, GlobalRowNo, Iteration = 0;
     double *ARecv, *BRecv, *Bloc_XX, *X_New, *X_Old, *Bloc_X, *Input_A, *Input_B;
     std::vector<double> res(n);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);
     amountRowBloc = n / size;    // how much to each proc
     Input_A = new double[n * n];
     Input_B = new double[n];
+    Bloc_X = new double[n];
+    X_New = new double[n];
+    X_Old = new double[n];
     for (int i = 0; i < n * n; i++) {
         Input_A[i] = A[i];
         if (i < n) {
             Input_B[i] = B[i];
+            X_New[i] = B[i];
         }
     }
     ARecv = new double[amountRowBloc * n];
@@ -81,19 +83,8 @@ std::vector<double> Parallel_Jacoby(std::vector<double> A, std::vector<double> B
     MPI_Scatter(Input_A, amountRowBloc * n, MPI_DOUBLE, ARecv, amountRowBloc * n, MPI_DOUBLE, 0,     MPI_COMM_WORLD);
     MPI_Scatter(Input_B, amountRowBloc, MPI_DOUBLE, BRecv, amountRowBloc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    Bloc_X = new double[n];
-    X_New = new double[n];
-    X_Old = new double[n];
-
     for (int irow=0; irow < amountRowBloc; irow ++) {
         Bloc_X[irow] = BRecv[irow];
-    }
-
-    MPI_Allgather(Bloc_X, amountRowBloc, MPI_DOUBLE, X_New, amountRowBloc, MPI_DOUBLE, MPI_COMM_WORLD);
-    int Iteration = 0;
-
-    for (int irow = amountRowBloc * size; irow < n; irow++) {
-        MPI_Allgather(&Input_B[irow], 1, MPI_DOUBLE, &X_New[irow], 1, MPI_DOUBLE, MPI_COMM_WORLD);
     }
 
     Bloc_XX = new double[n];
