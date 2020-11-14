8775861575b6ee0e003e449adc7d93966a0a426a
KustovaAI <kustova_nastenka@list.ru>
2020-11-14 12:29:39 -0500

    deleted

--- modules/task_2/kustova_a_jacoby/start.sh
+++
@@ -1,8 +0,0 @@
-NAME=$(pwd|tr / "\n"|tail |tail -1)_mpi
-cd ../../../build
-cmake -D USE_SEQ=ON -D USE_MPI=ON -D USE_OMP=ON -D USE_TBB=ON -D USE_STD=ON -D USE_LATEX=ON ..
-cmake --build . --config RELEASE
-cd bin/
-#./$NAME
-
-mpiexec -n 3 $NAME
