7cf67e64d08f2d62a2854469e54a9f4160fa077c
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-11-14 16:27:12 +0300

    comments

--- modules/task_2/kryukov_s_topology_star/topology_star.h
+++ modules/task_2/kryukov_s_topology_star/topology_star.h
@@ -5,6 +5,10 @@
 #include <vector>
 #include <string>
 
-
+void initialVectorNodes(std::vector<int>& inputV);
+MPI_Comm createTopologyStar(std::vector<int> MasNodes);
+void createMasEdges(std::vector<int> inputV, std::vector<int>& edges, std::vector<int>& index);
+int starSend(void *buf, int count, MPI_Datatype datatype, int dest, int send_tag, MPI_Comm comm);
+int starRecv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status);
 
 #endif  // MODULES_TASK_2_KRYUKOV_S_TOPOLOGY_STAR_TOPOLOGY_STAR_H_
