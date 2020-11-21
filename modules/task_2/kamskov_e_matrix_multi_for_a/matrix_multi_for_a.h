882912fb0d9eb453cf320fa9b3380d1c3c5fb144
Eugene Kamskov <32013883+KamskovEM@users.noreply.github.com>
2020-11-21 20:28:31 +0300

    local lint crazy

--- modules/task_2/kamskov_e_matrix_multi_for_a/matrix_multi_for_a.h
+++ modules/task_2/kamskov_e_matrix_multi_for_a/matrix_multi_for_a.h
@@ -1,7 +1,6 @@
 // Copyright 2020 Kamskov Eugene
-#ifndef PP_2020_AUTUMN_ENGINEER_MODULES_TASK_2_KAMSKOV_E_MATRIX_MULTI_FOR_A_MATRIX_MULTI_FOR_A_H_
-#define PP_2020_AUTUMN_ENGINEER_MODULES_TASK_2_KAMSKOV_E_MATRIX_MULTI_FOR_A_MATRIX_MULTI_FOR_A_H_
-
+#ifndef MODULES_TASK_2_KAMSKOV_E_MATRIX_MULTI_FOR_A_MATRIX_MULTI_FOR_A_H_
+#define MODULES_TASK_2_KAMSKOV_E_MATRIX_MULTI_FOR_A_MATRIX_MULTI_FOR_A_H_
 #include <vector>
 
 std::vector<int> getRandomMatrix(int rows, int columns);
@@ -9,4 +8,4 @@ std::vector<int> getParallelOperations(std::vector<int> global_vec, std::vector<
     int rows);
 std::vector<int> getSequentialOperations(std::vector<int> vec, std::vector<int> sec_vec, int size_row);
 std::vector<int> getRes(std::vector<int> vec, std::vector<int> sec_vec, int size_row);
-#endif  // PP_2020_AUTUMN_ENGINEER_MODULES_TASK_2_KAMSKOV_E_MATRIX_MULTI_FOR_A_MATRIX_MULTI_FOR_A_H_
+#endif  // MODULES_TASK_2_KAMSKOV_E_MATRIX_MULTI_FOR_A_MATRIX_MULTI_FOR_A_H_
