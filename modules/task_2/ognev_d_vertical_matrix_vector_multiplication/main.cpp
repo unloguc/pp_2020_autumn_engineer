d7eff12e255bc3c3c95e1674923f1d01a7149295
DenisOgnev <denisognev@icloud.com>
2020-11-23 21:53:45 +0300

    fix

--- modules/task_2/ognev_d_vertical_matrix_vector_multiplication/main.cpp
+++ modules/task_2/ognev_d_vertical_matrix_vector_multiplication/main.cpp
@@ -2,64 +2,99 @@
 
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
-#include "../../modules/task_2/ognev_d_vertical_matrix_vector_multiplication/multiplication.h"
+#include "../../modules/task_1/ognev_d_vertical_matrix_vector_multiplication/multiplication.h"
 
-/*TEST(Sequential_Multiplication_Test, Throws_When_Matrix_Is_Empty) {
-  int rows = 5, columns = 5, vectorSize = columns;
-  int* matrix = nullptr;
-  int* vector = getRandomVector(vectorSize);
-  ASSERT_ANY_THROW(sequentialMultiplication2(matrix, vector, rows, columns, vectorSize));
-}
-
-TEST(Sequential_Multiplication_Test, Throws_When_Vector_Is_Empty) {
-  int rows = 5, columns = 5, vectorSize = columns;
+TEST(Parallel_Multiplication_Test, Works_On_1x1_Matrix) {
+  int rows = 1, columns = 1, vectorSize = columns;
   int* matrix = getRandomMatrix(rows, columns);
-  int* vector = nullptr;
-  ASSERT_ANY_THROW(sequentialMultiplication2(matrix, vector, rows, columns, vectorSize));
+  int* vector = getRandomVector(vectorSize); 
+  ASSERT_NO_THROW(parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize));
 }
 
-TEST(Sequential_Multiplication_Test, Throws_When_Number_Of_Matrix_Columns_Are_Not_Equal_To_Number_Of_Vector_Rows) {
-  int rows = 5, columns = 5, vectorSize = 6;
+TEST(Parallel_Multiplication_Test, Works_On_1x5_Matrix) {
+  int rows = 1, columns = 5, vectorSize = columns;
   int* matrix = getRandomMatrix(rows, columns);
-  int* vector = getRandomVector(vectorSize);
-  ASSERT_ANY_THROW(sequentialMultiplication2(matrix, vector, rows, columns, vectorSize));
+  int* vector = getRandomVector(vectorSize); 
+  ASSERT_NO_THROW(parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize));
 }
 
-TEST(Parallel_Multiplication_Test, Throws_When_Matrix_Is_Empty) {
-  int rows = 5, columns = 5, vectorSize = columns;
-  int* matrix = nullptr;
-  int* vector = getRandomVector(vectorSize);
-  ASSERT_ANY_THROW(parallelMultiplication(matrix, vector, rows, columns, vectorSize));
-}
-
-TEST(Parallel_Multiplication_Test, Throws_When_Vector_Is_Empty) {
-  int rows = 5, columns = 5, vectorSize = columns;
+TEST(Parallel_Multiplication_Test, Works_On_5x1_Matrix) {
+  int rows = 5, columns = 1, vectorSize = columns;
   int* matrix = getRandomMatrix(rows, columns);
-  int* vector = nullptr;
-  ASSERT_ANY_THROW(parallelMultiplication(matrix, vector, rows, columns, vectorSize));
+  int* vector = getRandomVector(vectorSize); 
+  ASSERT_NO_THROW(parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize));
 }
 
-TEST(Parallel_Multiplication_Test, Throws_When_Number_Of_Matrix_Columns_Are_Not_Equal_To_Number_Of_Vector_Rows) {
-  int rows = 5, columns = 5, vectorSize = 6;
+TEST(Parallel_Multiplication_Test, Parallel_Equals_Sequential_On_1x1_Matrix) {
+  int rows = 1, columns = 1, vectorSize = columns;
   int* matrix = getRandomMatrix(rows, columns);
   int* vector = getRandomVector(vectorSize);
-  ASSERT_ANY_THROW(parallelMultiplication(matrix, vector, rows, columns, vectorSize));
-}
-
-TEST(Multiplication_Test, Parallel_Equals_To_Sequential) {
-  int rows = 5, columns = 6, vectorSize = columns;
-  int* matrix1 = getRandomMatrix(rows, columns);
-  int* vector1 = getRandomVector(vectorSize);
-  int* sequentialResult = sequentialMultiplication2(matrix1, vector1, rows, columns, vectorSize);
-  int* parallelResult = parallelMultiplication(matrix1, vector1, rows, columns, vectorSize);
+  int* sequentialResult = sequentialTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  int* parallelResult = parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
   for (int i = 0; i < rows; i++) {
     EXPECT_EQ(sequentialResult[i], parallelResult[i]);
   }
-}*/
+}
+
+TEST(Parallel_Multiplication_Test, Parallel_Equals_Sequential_On_1x5_Matrix) {
+  int rows = 1, columns = 5, vectorSize = columns;
+  int* matrix = getRandomMatrix(rows, columns);
+  int* vector = getRandomVector(vectorSize);
+  int* sequentialResult = sequentialTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  int* parallelResult = parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  for (int i = 0; i < rows; i++) {
+    EXPECT_EQ(sequentialResult[i], parallelResult[i]);
+  }
+}
+
+TEST(Parallel_Multiplication_Test, Parallel_Equals_Sequential_On_5x1_Matrix) {
+  int rows = 5, columns = 1, vectorSize = columns;
+  int* matrix = getRandomMatrix(rows, columns);
+  int* vector = getRandomVector(vectorSize);
+  int* sequentialResult = sequentialTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  int* parallelResult = parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  for (int i = 0; i < rows; i++) {
+    EXPECT_EQ(sequentialResult[i], parallelResult[i]);
+  }
+}
+
+TEST(Parallel_Multiplication_Test, Parallel_Equals_Sequential_On_3x3_Matrix) {
+  int rows = 3, columns = 3, vectorSize = columns;
+  int* matrix = getRandomMatrix(rows, columns);
+  int* vector = getRandomVector(vectorSize);
+  int* sequentialResult = sequentialTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  int* parallelResult = parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  for (int i = 0; i < rows; i++) {
+    EXPECT_EQ(sequentialResult[i], parallelResult[i]);
+  }
+}
+
+TEST(Parallel_Multiplication_Test, Parallel_Equals_Sequential_On_3x4_Matrix) {
+  int rows = 3, columns = 4, vectorSize = columns;
+  int* matrix = getRandomMatrix(rows, columns);
+  int* vector = getRandomVector(vectorSize);
+  int* sequentialResult = sequentialTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  int* parallelResult = parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  for (int i = 0; i < rows; i++) {
+    EXPECT_EQ(sequentialResult[i], parallelResult[i]);
+  }
+}
+
+TEST(Parallel_Multiplication_Test, Parallel_Equals_Sequential_On_4x3_Matrix) {
+  int rows = 4, columns = 3, vectorSize = columns;
+  int* matrix = getRandomMatrix(rows, columns);
+  int* vector = getRandomVector(vectorSize);
+  int* sequentialResult = sequentialTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  int* parallelResult = parallelTransposedMultiplication(matrix, vector, rows, columns, vectorSize);
+  for (int i = 0; i < rows; i++) {
+    EXPECT_EQ(sequentialResult[i], parallelResult[i]);
+  }
+}
 
 int main(int argc, char** argv) {
-  /*::testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   MPI_Init(&argc, &argv);
+  
 
   ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
   ::testing::TestEventListeners& listeners =
@@ -69,10 +104,5 @@ int main(int argc, char** argv) {
   listeners.Release(listeners.default_xml_generator());
 
   listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-  return RUN_ALL_TESTS();*/
-    int rows = 2, columns = 3, vectorSize = columns, matrixSize = rows * columns;
-
-    int* matrix = getRandomMatrix(rows, columns);
-    int* vector = getRandomVector(vectorSize);
-    matrixTransposition(matrix, rows, columns);
+  return RUN_ALL_TESTS();
 }
