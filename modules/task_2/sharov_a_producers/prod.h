a2fda02c484de2b7c5bd742b290b9361758ce231
Alexander <sharoffalex@gmail.com>
2020-11-23 14:23:36 +0300

    release 1

---
+++ modules/task_2/sharov_a_producers/prod.h
@@ -0,0 +1,16 @@
+// Copyright 2020 Alexander Sharov
+#ifndef MODULES_TASK_2_SHAROV_A_PRODUCERS_H_
+#define MODULES_TASK_2_SHAROV_A_PRODUCERS_H_
+
+#include <string>
+#include <queue>
+
+void sleep_seconds(double seconds);
+
+int master(int loops);
+
+void produce();
+
+void consume();
+
+#endif  // MODULES_TASK_2_MISHIN_I_PRODUCERS_PRODUCERS_H_
