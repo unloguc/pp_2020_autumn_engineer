fcc32fd3d6c74bf7c6a27af93e781c4a74f09d12
alyona-eryomina <alyona.eryomina01@gmail.com>
2020-12-05 23:17:47 +0300

    Fixed

--- modules/task_2/eremina_a_ring/ring.cpp
+++ modules/task_2/eremina_a_ring/ring.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Eremina Alena
+// Copyright 2020 Eremina Alena
 #include "../../../modules/task_2/eremina_a_ring/ring.h"
 
 MPI_Comm topologyRing(int* order) {
@@ -13,9 +13,20 @@ MPI_Comm topologyRing(int* order) {
 
     int *edges = new int[ProcNum * 2];
     if (order != NULL) {
+        int left, right;
         for (int i = 0; i < ProcNum; i++) {
-            edges[(2 + (i * 2)) % (ProcNum * 2)] = order[i];
-            edges[(ProcNum * 2 - 1 + i * 2) % (ProcNum * 2)] = order[i];
+            if (i == 0) {
+                left = order[ProcNum-1];
+            } else {
+                left = order[i - 1];
+            }
+            if (i == ProcNum - 1) {
+                right = order[0];
+            } else {
+                right = order[i + 1];
+            }
+            edges[2 * order[i]] = left;
+            edges[2 * order[i] + 1] = right;
         }
     } else {
         for (int i = 0; i < ProcNum; i++) {
@@ -25,7 +36,7 @@ MPI_Comm topologyRing(int* order) {
     }
 
     MPI_Comm RingComm;
-    MPI_Graph_create(MPI_COMM_WORLD, ProcNum, index, edges, 1, &RingComm);
+    MPI_Graph_create(MPI_COMM_WORLD, ProcNum, index, edges, true, &RingComm);
 
     return RingComm;
 }
